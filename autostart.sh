13d443ff98a185cb8f26e6565aa42b331eaec9ac
ybygjylj <abc1870819884@163.com>
2020-08-15 11:02:37 +0800

    first-commit

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/bin/bash ~/scripts/dwm-status.sh &
+/bin/bash ~/scripts/wp-autochange.sh &
+picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
+/bin/bash ~/scripts/tap-to-click.sh &
+/bin/bash ~/scripts/inverse-scroll.sh &
+nm-applet &
+xfce4-volumed-pulse &
+/bin/bash ~/scripts/run-mailsync.sh &
+~/scripts/autostart_wait.sh &
+albert &
+ss-qt5 &
+fcitx &
